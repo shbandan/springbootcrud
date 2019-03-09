ad62d9bfe8d474ec5ee5f20a4222466059f538b2
vishalranapariya <vishalranapariya@vishals-mbp.fios-router.home>
2019-03-08 19:41:07 -0500

    added new code

---
+++ src/main/java/com/demo/UserDetailController.java
@@ -0,0 +1,29 @@
+package com.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userdetail")
+public class UserDetailController {
+
+	@Autowired
+	UserdetailRepository userdetailRepository;
+	
+	@PostMapping()
+	public Userdetail createUser(@RequestBody Userdetail userdetail) {
+		return userdetailRepository.save(userdetail);
+	}
+	
+	@GetMapping()
+	public List<Userdetail> getAllUser(){
+		return userdetailRepository.findAll();
+	}
+	
+}
---
+++ src/main/java/com/demo/Userdetail.java
@@ -0,0 +1,66 @@
+package com.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "userdetail")
+public class Userdetail {
+
+	@Id
+	@GeneratedValue(generator = "userdetail_generator")
+	@SequenceGenerator(name = "userdetail_generator", sequenceName = "quserdetail_sequence", initialValue = 1000)
+	private Long id;
+
+	private String firstname;
+
+	private String lastname;
+
+	private Long phoneNo;
+
+	private String city;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(Long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
