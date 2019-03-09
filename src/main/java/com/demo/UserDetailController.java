9586e0dbca687c131dec850bdc70cf5ce3c82587
shbandan <47702808+shbandan@users.noreply.github.com>
2019-03-08 21:24:36 -0500

    Update UserDetailController.java

--- src/main/java/com/demo/UserDetailController.java
+++ src/main/java/com/demo/UserDetailController.java
@@ -2,13 +2,17 @@ package com.demo;
 
 import java.util.List;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/userdetail")
 public class UserDetailController {
